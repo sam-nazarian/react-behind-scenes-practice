b1294b13235d6b8a15885ada7d743b67e7edc56c
samanfathnazarian <saman.nazarian17@gmail.com>
2023-01-27 13:20:39 -0600

    useCallback() and its Dependencies

--- src/App.js
+++ src/App.js
@@ -6,21 +6,30 @@ import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
 
   // func will be the same in memory cause of useCallback
   // The memoized version of the function will only change if one of the dependencies has changed.
   const toggleParagraphHandler = useCallback(() => {
-    // set the state to the opposite value of the prevState
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
+    // values that come from outside the function should be set as a dependency
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph); // set the state to the opposite value of the prevState
+    }
+  }, [allowToggle]);
+  // if allowToggle changes then recreate the func & store the recreated func
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   // even though there's no prop changing in the child components they're still re-executed like function calls
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle paragraph!</Button>
     </div>
   );
